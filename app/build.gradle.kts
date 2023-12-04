797924e717caea0a8c9dfcaeae8a9fe4e4aca97d
luqmanadi <78971486+luqmanadi@users.noreply.github.com>
2023-12-04 17:28:27 +0700

    Add Splash Screen
    Tambah splash screen ke thema

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,11 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures{
+        viewBinding = true
+        android.buildFeatures.buildConfig = true
+    }
 }
 
 dependencies {
@@ -44,4 +49,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // splash screen
+    implementation("androidx.core:core-splashscreen:1.0.1")
+    
 }
--- app/src/main/java/com/bangkit/motix/MainActivity.kt
+++ app/src/main/java/com/bangkit/motix/MainActivity.kt
@@ -2,10 +2,21 @@ package com.bangkit.motix
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.bangkit.motix.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
